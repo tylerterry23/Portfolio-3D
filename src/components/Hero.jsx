49863715142bbc73ef3d63ce598c5b7f7581d49f
Tyler Terry <tvp7098@wmich.edu>
2023-07-06 20:31:38 -0400

    title, favicon, nav, hero, 3d model

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,33 @@
-import React from 'react'
+import { motion } from 'framer-motion';
+
+import { styles } from '../styles';
+import { ComputersCanvas } from './canvas'
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <section className='relative w-full h-screen mx-auto'>
+      <div className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex items-start gap-5`}>
+
+        {/* Ball and line design */}
+        <div className='flex flex-col justify-center items-center mt-5'>
+          <div className='w-5 h-5 rounded-full bg-[#915eff]'/>
+          <div className='w-1 sm:h-80 h-40 violet-gradient bg-[#ff4d4d]'/>
+        </div>
+
+        {/* Text */}
+        <div>
+          <h1 className={`${styles.heroHeadText} text-white`}>
+            Hi, I'm <span className='text-[#915eff]'>Tyler</span>
+          </h1>
+
+          <p className={`${styles.heroSubText} mt-2 text-white-100`}>
+            Lorem, ipsum dolor sit amet  <br className="sm:block hidden"/> tempore mollitia inventore ad?
+          </p>
+        </div>
+      </div>
+
+      <ComputersCanvas />
+    </section>
   )
 }
 
